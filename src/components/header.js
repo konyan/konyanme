15505cc617d4667ccb05b7e278c73dc32da64365
konyan <devkonyan@gmail.com>
2020-05-03 02:21:26 +0630

    add header and css style

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,27 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styles from "./header.module.scss"
+import logo from "../images/logo.svg"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = () => (
+  <header className={styles.container}>
+    <nav className={styles.navbar}>
+      <div className={styles.navbar__header}>
+        <img src={logo} alt="konyan's blog logo" />
+      </div>
+      <ul className={styles.navbar__right}>
+        <li className={styles.navbar__right_item}>
+          <Link to="/"> Home </Link>
+        </li>
+        <li className={styles.navbar__right_item}>
+          <Link to="/"> Projects </Link>
+        </li>
+        <li className={styles.navbar__right_item}>
+          <Link to="/"> Resume </Link>
+        </li>
+      </ul>
+    </nav>
   </header>
 )
 
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import elkTheme from "typography-theme-elk-glen"
+
+const typography = new Typography(elkTheme)
+
+export const { scale, rhythm, options } = typography
+export default typography
