3f73e804fcb6ea99d0f2396d97dbde07ebdb3b0b
konyan <devkonyan@gmail.com>
2020-05-04 11:59:42 +0630

    add github link

--- src/components/card.js
+++ src/components/card.js
@@ -2,21 +2,23 @@ import PropTypes from "prop-types"
 import React from "react"
 import styles from "./card.module.scss"
 
-const Card = ({ title, skills, image }) => {
+const Card = ({ title, skills, image, github }) => {
   return (
-    <div className={styles.container}>
+    <a className={styles.container} href={github} target="blank">
       <div className={styles.image__container}>
         <img src={image} alt={title} className={styles.image__container_img} />
       </div>
       <div className={styles.body__container}>
         <ul className={styles.body__container__skills}>
           {skills.map(skill => (
-            <li className={styles.body__container__skills_item}>{skill}</li>
+            <li className={styles.body__container__skills_item} key={skill}>
+              {skill}
+            </li>
           ))}
         </ul>
         <h3 className={styles.body__container_title}>{title}</h3>
       </div>
-    </div>
+    </a>
   )
 }
 
@@ -24,6 +26,7 @@ Card.propTypes = {
   title: PropTypes.string.isRequired,
   skills: PropTypes.array.isRequired,
   image: PropTypes.string.isRequired,
+  github: PropTypes.string.isRequired,
 }
 
 Card.defaultProps = {
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,85 +8,72 @@ import Card from "../components/card"
 const projects = [
   {
     image:
-      "https://cdn.pixabay.com/photo/2017/04/03/15/52/love-you-2198772__340.png",
+      "https://raw.githubusercontent.com/konyan/love-note/development/app/assets/images/ss.png",
     title: "My Love Note",
     skills: ["Rails", "HTML", "CSS"],
     github: "https://github.com/konyan/love-note",
   },
   {
     image:
-      "https://cdn.pixabay.com/photo/2017/04/03/15/52/love-you-2198772__340.png",
+      "https://github.com/konyan/ruby-job-scrapper/raw/master/images/program.png",
     title: "Console Jobs",
     skills: ["Ruby", "Terminal"],
-    github: "https://github.com/konyan/love-note",
+    github: "https://github.com/konyan/ruby-job-scrapper",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2017/04/03/15/52/love-you-2198772__340.png",
-    title: "My Love Note",
-    skills: ["Rails", "HTML", "CSS"],
-    github: "https://github.com/konyan/love-note",
-  },
-  {
-    image:
-      "https://cdn.pixabay.com/photo/2017/04/03/15/52/love-you-2198772__340.png",
-    title: "My Love Note",
-    skills: ["Rails", "HTML", "CSS"],
-    github: "https://github.com/konyan/love-note",
-  },
-  {
-    image:
-      "https://cdn.pixabay.com/photo/2017/04/03/15/52/love-you-2198772__340.png",
-    title: "Console Jobs",
-    skills: ["Ruby", "Terminal"],
-    github: "https://github.com/konyan/love-note",
-  },
-  {
-    image:
-      "https://cdn.pixabay.com/photo/2017/04/03/15/52/love-you-2198772__340.png",
-    title: "My Love Note",
-    skills: ["Rails", "HTML", "CSS"],
-    github: "https://github.com/konyan/love-note",
+    title: "My Budget App",
+    skills: ["Javascript", "HTML", "CSS"],
+    github: "https://github.com/konyan/js-budget",
   },
 ]
 
-const Projects = () => (
-  <Layout>
-    <SEO title="Projects" />
-    <div className={styles.header}>
-      <h2 className={styles.header_first}>Who i am</h2>
-      <hr />
-      <h2 className={styles.header_second}>(today)</h2>
-    </div>
-    <div className={styles.skills}>
-      <ul className={styles.skills__list}>
-        <li className={styles.skills__list_item}>
-          <Link
-            to="/projects?skills=all"
-            activeClassName="active"
-            partiallyActive={true}
-          >
-            ALL
-          </Link>
-        </li>
-        <li className={styles.skills__list_item}>
-          <Link to="/projects?skills=html" activeClassName="active">
-            HTML
-          </Link>
-        </li>
-        <li className={styles.skills__list_item}>
-          <Link to="/projects?skills=css" activeClassName="active">
-            CSS
-          </Link>
-        </li>
-      </ul>
-    </div>
-    <div className={styles.cards}>
-      {projects.map(p => (
-        <Card title={p.title} skills={p.skills} image={p.image} key={p.title} />
-      ))}
-    </div>
-  </Layout>
-)
+const Projects = () => {
+  return (
+    <Layout>
+      <SEO title="Projects" />
+      <div className={styles.header}>
+        <h2 className={styles.header_first}>Who i am</h2>
+        <hr />
+        <h2 className={styles.header_second}>(today)</h2>
+      </div>
+      <div className={styles.skills}>
+        <ul className={styles.skills__list}>
+          <li className={styles.skills__list_item}>
+            <Link
+              to="/projects?skills=all"
+              activeClassName="active"
+              partiallyActive={true}
+            >
+              ALL
+            </Link>
+          </li>
+          <li className={styles.skills__list_item}>
+            <Link to="/projects?skills=html" activeClassName="active">
+              HTML
+            </Link>
+          </li>
+          <li className={styles.skills__list_item}>
+            <Link to="/projects?skills=css" activeClassName="active">
+              CSS
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.cards}>
+        {projects.map(p => (
+          <Card
+            title={p.title}
+            skills={p.skills}
+            image={p.image}
+            key={p.title}
+            github={p.github}
+          />
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export default Projects
